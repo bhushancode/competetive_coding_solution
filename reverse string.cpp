5f028091e84e9974e6538c77d0f24e04802cadb7
bhushancode <59665012+bhushancode@users.noreply.github.com>
2021-08-17 10:01:54 +0530

    Create reverse string.cpp

---
+++ reverse string.cpp
@@ -0,0 +1,10 @@
+    void reverseString(vector<char>& s) {
+        for(auto i=0; i<(s.size()/2);i++){
+            char temp=s[i];
+             s[i]=s[s.size()-1-i];
+            s[s.size()-1-i]=temp;
+            
+            // cout<<*i<<s[0];
+        }
+        
+    }
